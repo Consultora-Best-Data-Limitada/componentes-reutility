68db081f679ae13b4c2ce84dc58a38c47d64f6c3
Rafael <rafacalderon1297@gmail.com>
2023-03-27 11:58:50 -0300

    prueba tipeos

--- dist/typings/textContainer.d.ts
+++ dist/typings/textContainer.d.ts
@@ -1,38 +1,29 @@
-// import type CSS from "csstype";
-// import type {AllowedComponentProps, ComponentCustomProps, VNode, VNodeProps} from "vue";
-//
-// export declare interface TextContainerProps {
-//   fontSize: CSS.FontSizeProperty<string | number>;
-//   padding: CSS.PaddingProperty<string | number>;
-//   lineHeight: CSS.LineHeightProperty<string | number>;
-//   textAlign: CSS.TextAlignProperty;
-//   textTransform: CSS.TextTransformProperty;
-//   color: CSS.ColorProperty | CustomColor;
-//   hoverColor: CSS.ColorProperty | CustomColor;
-//   activeColor: CSS.ColorProperty | CustomColor;
-//   hoverBackground: CSS.BackgroundColorProperty | CustomColor;
-//   activeBackground: CSS.BackgroundColorProperty | CustomColor;
-//   whiteSpace: CSS.WhiteSpaceProperty;
-//   fontWeight: string | number;
-//   letterSpacing: CSS.LetterSpacingProperty<string | number>;
-//   fontFamily: CSS.FontFamilyProperty;
-//   predefinedStyle: PredefinedStyle;
-//   clickable: boolea;
-//   borderRadius: CSS.BorderRadiusProperty<string>;
-//   onClik: EventPro<(ev: MouseEvent) => void>;
-// }
-//
-// export declare const TextContainer: new () => {
-//   $props: AllowedComponentProps & ComponentCustomProps & VNodeProps & TextContainerProps;
-//   $slots: {
-//     default?: (({Component}: {
-//       Component: VNode;
-//     }) => VNode[]) | undefined;
-//   };
-// };
-
-import type {DefineComponent} from "vue";
+import type CSS from "csstype";
+import type {VNode, DefineComponent} from "vue";
 
 declare const TextContainer: DefineComponent<{
+  fontSize: CSS.FontSizeProperty<string | number>;
+  padding: CSS.PaddingProperty<string | number>;
+  lineHeight: CSS.LineHeightProperty<string | number>;
+  textAlign: CSS.TextAlignProperty;
+  textTransform: CSS.TextTransformProperty;
+  color: CSS.ColorProperty | CustomColor;
+  hoverColor: CSS.ColorProperty | CustomColor;
+  activeColor: CSS.ColorProperty | CustomColor;
+  hoverBackground: CSS.BackgroundColorProperty | CustomColor;
+  activeBackground: CSS.BackgroundColorProperty | CustomColor;
+  whiteSpace: CSS.WhiteSpaceProperty;
+  fontWeight: string | number;
+  letterSpacing: CSS.LetterSpacingProperty<string | number>;
+  fontFamily: CSS.FontFamilyProperty;
+  predefinedStyle: PredefinedStyle;
+  clickable: boolea;
+  borderRadius: CSS.BorderRadiusProperty<string>;
   onClik: (ev: MouseEvent) => void;
+  "@click": (ev: MouseEvent) => void;
+  $slots: {
+    default?: (({Component}: {
+      Component: VNode;
+    }) => VNode[]) | undefined;
+  };
 }>;
