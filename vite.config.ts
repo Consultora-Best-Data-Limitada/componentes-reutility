ecaa4fd6231de65d5d930d604643ed6b94dd3e71
Rafael <rafacalderon1297@gmail.com>
2023-03-27 11:46:43 -0300

    prueba tipeos

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import {fileURLToPath, URL} from 'node:url';
 
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    // dts(),
   ],
   resolve: {
     alias: {
