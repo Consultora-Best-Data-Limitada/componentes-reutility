810108bac135871aac07c4791e2a6dc280a68036
Rafael <rafacalderon1297@gmail.com>
2023-03-27 11:43:19 -0300

    prueba tipeos

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,25 +1,12 @@
-import {AllowedComponentProps, App, ComponentCustomProps, VNode, VNodeProps} from "vue";
+import type {App} from "vue";
+
+import {TextContainer} from "./typings/textContainer";
 
 export declare interface ComponentesReutility {
   install(app: App): void;
 }
 
 export declare function componentesReutility(): ComponentesReutility;
-
-export declare interface TextContainerProps {
-  name?: string;
-  textAlign?: string;
-}
-
-export declare const TextContainer: new () => {
-  $props: AllowedComponentProps & ComponentCustomProps & VNodeProps & TextContainerProps;
-  $slots: {
-    default?: (({Component, route}: {
-      Component: VNode;
-      route: RouteLocationNormalizedLoaded;
-    }) => VNode[]) | undefined;
-  };
-};
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
